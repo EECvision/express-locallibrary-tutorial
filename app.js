1b51b01d7a0d34f6c7a51dc4705af999e6c31016
EECvision <eec.studies@gmail.com>
2021-11-21 02:37:05 +0100

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ var app = express();
 
 app.use(helmet());
 
-//Set up mongoose connection
-var mongoDB = 'mongodb+srv://emmanuel:backend@backend.rkhc6.mongodb.net/local_library?retryWrites=true&w=majority';
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
